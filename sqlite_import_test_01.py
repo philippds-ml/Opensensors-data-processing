fc0dbad671d262e67207d5e493363add9ec4bfcf
Philipp Dominic Siedler <p.d.siedler@gmail.com>
2018-05-16 16:19:18 +0100

    change

--- sqlite_import_test_01.py
+++ sqlite_import_test_01.py
@@ -3,7 +3,6 @@ import sqlite3
 import pandas as pd
 import requests
 
-
 # pull JWT
 url_JWT = 'https://auth.opensensors.com/auth/login'
 headers_JWT = { 'x-api-key': 'UoheJ3fp0w7CJisPi26NzNOw2rEPyMj67ovksMo1' }
@@ -21,16 +20,19 @@ parameters = {'fromDate': '2018-02-04',
 
 os_data_request = requests.get(url_GPM, headers = headers_GPM, params = parameters).json()
 
-# create sqlite database
+##############################################################################
+# CREATE DATABASE CONNECTION
 
-# create table in database
-# creating sqlite task
+conn = sqlite3.connect("os_reading_AUB_01.sqlite")
+c = conn.cursor()
 table_name = '\'' + str(os_data_request["nextCursor"]) + '\''
-
 data = os_data_request['items'][0]
 heatmap_len = len(data['heatmap'])
 x_len = data['heatmap'][0]
 y_len = data['heatmap'][1]
+
+##############################################################################
+# CREATE DATABASE + TABLE
 
 heatmap_values = [''] * (heatmap_len - 2)
 for i in range(0, heatmap_len - 2):
@@ -47,10 +49,10 @@ sql_create_table = """ CREATE TABLE IF NOT EXISTS """ + table_name + """ (
                                         y_res INTEGER,
                                         """ + str(heatmap_values) + """); """
 
-# create a database connection
-conn = sqlite3.connect("os_reading_AUB_01.sqlite")
-c = conn.cursor()
 c.execute(sql_create_table)
+
+##############################################################################
+# INSERT DATA
 
 heatmap_column_names = []
 for v in range(0, len(data['heatmap']) - 2):
